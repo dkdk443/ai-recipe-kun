d140609c43c5188e0838c4888d277b993393d560
dkdk443 <demo_weeeds_470@yahoo.co.jp>
2024-09-29 17:03:40 +0900

    tailwind.css

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gradient-to-bl from-blue-50 to-fuchsia-100">
+      <div className="flex flex-row">
+        <div className="basis-2/5">01</div>
+        <div className="basis-3/5">02</div>
+      </div>
+    </div>
   )
 }
 
